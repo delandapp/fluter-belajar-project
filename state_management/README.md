52a454bcc56f4f7e77242877a8baf244de995847
delandapp <alihandronikolas@gmail.com>
2024-05-03 20:37:47 +0700

    update state management 1

---
+++ state_management/README.md
@@ -0,0 +1,3 @@
+# state_management
+
+A new Flutter project.
---
+++ state_management/android/app/src/main/kotlin/com/example/state_management/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.state_management
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
