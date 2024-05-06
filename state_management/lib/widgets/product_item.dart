63d4b57446a62cda643593188dc078a816a88cea
delandapp <alihandronikolas@gmail.com>
2024-05-06 12:34:53 +0700

    update comsummer pada provider

--- state_management/lib/widgets/product_item.dart
+++ state_management/lib/widgets/product_item.dart
@@ -8,18 +8,25 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final productData = Provider.of<Product>(context, listen: true);
+    final productData = Provider.of<Product>(context, listen: false);
+
+    //* Nah gimana kalau kita buat yang listen hanya iconButton kan karena yang di render mengulang hanya iconButtonya
+    //* kita bisa membungkus nya dengan widget Consumer (listen nya akan selalu true)
+    print("WIDGET REBUILD");
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
+          leading: Consumer<Product>(
+            builder: (context, productData, child) => IconButton(
             icon: (productData.isFavorite) ? const Icon(Icons.favorite) : const Icon(Icons.favorite_border_outlined),
             color: Theme.of(context).colorScheme.secondary,
             onPressed: () {
               productData.statusFav();
             },
+          ),
           ),
           title: Text(
             productData.title.toString(),
